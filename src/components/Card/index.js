719db59c97e5de8580313cfe89635c38528b81c8
Pedro Correa <pedrocorrea.85@hotmail.com>
2019-08-05 23:21:51 -0300

    S1 D1 - Tela Home

---
+++ src/components/Card/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, Image, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
+import style from './style';
+
+const Card = ({ title, description }) => (
+  <View>
+    <View style={style.container}>
+      <TouchableOpacity style={style.card}>
+        <View style={{ flex: 1 }}>
+          <Text style={style.title}>{title}</Text>
+          <Text style={style.description}>{description}</Text>
+        </View>
+        <View style={{ flex: 1, alignSelf: 'center' }}>
+          <Image
+            style={style.cardImage}
+            source={{
+              uri:
+                'https://aleteiaportuguese.files.wordpress.com/2017/05/web3-our-lady-of-fatima-pilgrm-statue-statue-church-stained-glass-blessed-virgin-dennis-callahan-photos-cc.jpg?quality=100&strip=all&w=620&h=310&crop=1'
+            }}
+          />
+        </View>
+      </TouchableOpacity>
+    </View>
+  </View>
+);
+
+export default Card;
+
+Card.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string
+};
--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { colors } from '../../styles';
+import Card from '../../components/Card';
 
 export function navigationOptionsHome() {
   return {
-    title: 'Início',
+    title: 'Seja bem vindo',
     headerStyle: {
       backgroundColor: colors.primary
     },
@@ -16,7 +16,17 @@ export function navigationOptionsHome() {
 }
 
 function Home() {
-  return <Text>Olá</Text>;
+  return (
+    <>
+      <Card title="Novena" description="Tudo pronto? Vamos iniciar a novena!" />
+      <Card title="Ensaio" description="Reúna a galera e vamos treinar para animar a novena!" />
+      <Card
+        title="Aprenda"
+        description="Fique por dentro de todas as orações e 
+      mistérios do Santo Terço"
+      />
+    </>
+  );
 }
 
 export default Home;
